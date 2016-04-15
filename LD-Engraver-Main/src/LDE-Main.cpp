338a67a046da037bbe99481e2307be4267ab3271
debian <dufrasne@hotmail.com>
2016-04-15 21:55:47 +0200

    1st commit

---
+++ LD-Engraver-Main/src/LDE-Main.cpp
@@ -0,0 +1,26 @@
+//
+// LDE-Main.cpp
+//
+// Master file
+//
+
+#include <iostream>
+/**
+ *
+ */
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+/**
+ *
+ * @param i
+ * @return
+ */
+int test(int i) {
+	return 1;
+}
